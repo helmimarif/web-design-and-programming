457b48d6e4a98a1b810572b441d4c9a08d6904b8
Ayse <106028948+helmimarif@users.noreply.github.com>
2022-05-23 10:36:04 +0700

    PHP

---
+++ 12. Latihan 2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variabel antar halaman </title>
+</head>
+<body>
+   <form action = "Variabel_hasil.php" method="POST">
+       <pre>
+           Nama : <input type="text" name="nama"> <br />
+           Email : <input type="text" name="email"> <br />
+           <input type = "submit" name="submit" value = "Tampilkan!";
+        </pre>
+    </form>
+</body>
+</html>
---
+++ 12. variabel_hasil.php
@@ -0,0 +1,8 @@
+<?php
+    echo $_POST ['nama'];
+    echo "<br />";
+    echo $_REQUEST ['nama'];
+    echo $_POST ['email'];
+    echo "<br />";
+    echo $_REQUEST ['email'];
+?>
